9cee5d8faaa828b75ea800963be0f7643c8184e5
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-10-23 18:05:07 -0700

    script update

--- Script Examples/listProperties.py
+++ Script Examples/listProperties.py
@@ -4,13 +4,13 @@ if selection.Count > 0:
     mi = selection[0]
     for pc in mi.PropertyCategories:
         print('\n')
-        print('Display Name: {}  Internal Name: {}'.format(pc.DisplayName, pc.Name))
-        print('\tProperties')
+        print('Property Category: {} ({})'.format(pc.DisplayName, pc.Name))
         for dp in pc.Properties:
-            print('\tDisplay Name: {}  Internal Name: {}'.format(dp.DisplayName, dp.Name))
             if dp.Value.IsDisplayString:
-                print('\t\t[Value]: {}'.format(dp.Value.ToString()))
+                value = dp.Value.ToString()
             elif dp.Value.IsDateTime:
-                print('\t\t[Value]: {}'.format(dp.Value.ToDateTime().ToShortTimeString()))
+                value = dp.Value.ToDateTime().ToShortTimeString()
             else:
-                print('\t\t[Value]: {}'.format(dp.Value.ToString()))
+                value = dp.Value.ToString()
+
+            print('\t{} ({}): {}'.format(dp.DisplayName, dp.Name, value))
