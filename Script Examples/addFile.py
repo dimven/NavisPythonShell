06662c23590caab476055332ee74c3acda5aa604
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-10-23 21:59:45 -0700

    Cleaned up script

--- Script Examples/addFile.py
+++ Script Examples/addFile.py
@@ -2,9 +2,9 @@
 path1 = r"path-to-your-file"
 
 try:
-	with doc.BeginTransaction("test") as t1:
-		doc.AppendFile(path1)
-		t1.Commit()
-		print "%s appended successfully" % path1
+    with doc.BeginTransaction("test") as t1:
+        doc.AppendFile(path1)
+        t1.Commit()
+        print "%s appended successfully" % path1
 except Exception, ex:
-	print str(ex)
+    print str(ex)
