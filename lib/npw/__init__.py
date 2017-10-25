52e9fee1fb27b22f347307287ac33401e9c2c7a5
Ehsan Iran-Nejad <eirannejad@gmail.com>
2017-10-25 08:36:15 -0700

    Moved npw into its own repo
    https://github.com/eirannejad/NavisPythonWrapper

--- lib/npw/__init__.py
+++
@@ -1,7 +0,0 @@
-import clr
-clr.AddReference('Autodesk.Navisworks.Api')
-import Autodesk.Navisworks.Api as API
-
-
-doc = API.Application.ActiveDocument
-selection = doc.CurrentSelection.SelectedItems
